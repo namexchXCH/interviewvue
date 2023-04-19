fcc52d8de8f4eb1a80507e2da743df080bdea887
xch <3252323572@qq.com>
2023-04-19 13:12:56 +0800

    XCH

---
+++ src/components/HomePage.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <div class="head" >
+      
+    </div>
+    <div class="center">
+
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HomePage",
+
+  data() {
+    return {};
+  },
+
+  mounted() {},
+
+  methods: {},
+};
+</script>
+
+<style lang="css" scoped>
+.head {
+  width: 100%;
+  height: 8vh;
+  background-color: #FFFFFF;
+ 
+}
+
+.center{
+  height: 92vh;
+  width: 100%;
+  background-image: linear-gradient(to top, #ffffff 0%, #E7F5F5);
+  box-shadow: 20px 0 0 #333333;
+  box-shadow: inset 0px 15px 15px -18px rgb(171, 204, 237);
+}
+</style>
