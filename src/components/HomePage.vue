ca72bee15c26684f6d24989d8a60d3ee5f2207ae
xch <3252323572@qq.com>
2023-04-20 17:40:23 +0800

    2

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -1,11 +1,32 @@
 <template>
   <div>
-    <div class="head" >
-      
+    <div class="head">
+      <div class="logo">
+        <img
+          src="../../public/mag/16UHJ4)6(8IX3MXHM[9XERU.png"
+          alt=""
+          class="logo"
+        />
+      </div>
+      <div class="xcv">
+        <el-menu
+        :default-active="activeIndex"
+          class="el-menu-demo"
+          mode="horizontal"
+          color="#000"
+          @select="handleSelect"
+        >
+          <el-menu-item index="1" class="kk">首页</el-menu-item>
+          <el-menu-item index="2" class="kk">面经</el-menu-item>
+          <el-menu-item index="3" class="kk">学习</el-menu-item>
+          <el-menu-item index="4" class="kk">哈哈哈</el-menu-item>
+        </el-menu>
+      </div>
+      <div class="right1">
+       
+      </div>
     </div>
-    <div class="center">
-
-    </div>
+    <div class="center"></div>
   </div>
 </template>
 
@@ -14,28 +35,78 @@ export default {
   name: "HomePage",
 
   data() {
-    return {};
+    return {
+      activeIndex: "1",
+    };
   },
 
   mounted() {},
 
-  methods: {},
+  methods: {
+    handleSelect(key, keyPath) {
+      console.log(key, keyPath);
+    },
+  },
 };
 </script>
 
 <style lang="css" scoped>
 .head {
+  display: flex;
   width: 100%;
   height: 8vh;
-  background-color: #FFFFFF;
- 
+  background-color: #ffffff;
+  align-items: center;
 }
 
-.center{
+.center {
   height: 92vh;
   width: 100%;
-  background-image: linear-gradient(to top, #ffffff 0%, #E7F5F5);
-  box-shadow: 20px 0 0 #333333;
+  background-image: linear-gradient(to top, #ffffff 0%, #e7f5f5);
   box-shadow: inset 0px 15px 15px -18px rgb(171, 204, 237);
+}
+
+.logo {
+  width: 90px;
+  height: 50px;
+  margin-left: 10%;
+}
+.xcv {
+  display: flex;
+  /* width: 30%; */
+  height: 50px;
+  margin-left: 10%;
+
+  align-items: center;
+  background-color: #e7f5f5;
+}
+.right1 {
+  width: 400px;
+  height: 50px;
+  margin-left: 30%;
+  background-color: #e7f5f5;
+}
+.t1 {
+  width: 50px;
+  height: 30px;
+  display: inline-block;
+  /* background-color: aquamarine; */
+  margin-left: 15px;
+  text-align: center;
+  line-height: 30px;
+}
+
+.t1:hover {
+  color: #2b9c7c;
+  cursor: pointer;
+}
+
+.t1:active {
+  color: #2b9c7c;
+}
+
+.kk{
+  font-size: 16px;
+  color: #000;
 }
 </style>
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,26 @@
 const { defineConfig } = require('@vue/cli-service')
+const path = require('path');
 module.exports = defineConfig({
-  transpileDependencies: true
+
+  transpileDependencies: true,
+  lintOnSave:false,
+  chainWebpack: (config) => {
+    // set svg-sprite-loader
+    config.module
+      .rule('svg')
+      .exclude.add(path.join(__dirname, './src/Icons/svg')) // 存放svg文件的目录
+      .end();
+    config.module
+      .rule('icons')
+      .test(/\.svg$/)
+      .include.add(path.join(__dirname, './src/Icons/svg')) // 存放svg文件的目录
+      .end()
+      .use('svg-sprite-loader')
+      .loader('svg-sprite-loader')
+      .options({
+        symbolId: 'icon-[name]'
+      })
+      .end();
+  }
+
 })
