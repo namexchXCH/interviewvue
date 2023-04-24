bcf5a4c0cabe8cf463c9d9902615a3e27485dfc3
xch <3252323572@qq.com>
2023-04-24 20:29:47 +0800

    zsdc

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -8,103 +8,152 @@
       </Col>
       <Col span="14">
         <Menu mode="horizontal" :theme="theme1" active-name="1">
-          <MenuItem name="1" @click="shouye">
-            <Icon type="ios-paper" color="#037CFF" size=16 />
+          <MenuItem name="1" @click.native="shouye">
+            <Icon type="ios-paper" color="#037CFF" size="16" />
             首页
           </MenuItem>
           <MenuItem name="2">
-            <Icon type="ios-book" color="#009961" size=16 />
+            <Icon type="ios-book" color="#009961" size="16" />
             面试八股
           </MenuItem>
           <MenuItem name="3">
-            <Icon type="ios-construct" color="#E5010F" size=16 />
+            <Icon type="ios-construct" color="#E5010F" size="16" />
             技能树
           </MenuItem>
           <Submenu name="4">
-            <template slot="title" >
-              <Icon type="ios-stats" color="#5FC948" size=16 />
+            <template slot="title">
+              <Icon type="ios-stats" color="#5FC948" size="16" />
               学习
             </template>
-            <MenuItem name="3-1"><Icon type="ios-paper" color="#419F4E" />课程推荐</MenuItem>
-            <MenuItem name="3-2"><Icon type="ios-paper" color="#0E4740"/>项目实战</MenuItem>
-            <MenuItem name="3-3"><Icon type="ios-paper" color="#FC8A15"/>书籍推荐</MenuItem>
-          </Submenu>
 
+            <MenuItem name="3-1"
+              ><Icon type="md-list-box" color="#419F4E" />课程推荐</MenuItem
+            >
+            <MenuItem name="3-2"
+              ><Icon type="md-cube" color="#0E4740" />项目实战</MenuItem
+            >
+            <MenuItem name="3-3"
+              ><Icon type="md-book" color="#FC8A15" />书籍推荐</MenuItem
+            >
+          </Submenu>
         </Menu>
       </Col>
 
       <Col span="6">
         <div class="topright">
-          <Row >
-            <Col span="6">col-8</Col>
-            <Col span="6">col-8</Col>
-            <Col span="6">col-8</Col>
-            <Col span="6">col-8</Col>
+          <Row class="tophead">
+            <Col span="6">
+              <MenuItem name="3-1" class="myitem"  @click.native="modal1 = true"
+                ><svg-icon iconClass="denlu" class="icon1"></svg-icon>登录注册</MenuItem
+              >
+            </Col>
+            <!-- <Button type="primary"
+              >Display dialog box</Button
+            > -->
+            <Modal v-model="modal1" >
+              <!-- <p slot="header" style="color: #f60; text-align: center"> </p> -->
+              <div style="text-align: center">
+                <LoginRegister></LoginRegister>
+            
+           
+              </div>
+              <div slot="footer">
+             
+              </div>
+            </Modal>
+      
+            <Col span="6">
+              <!-- <MenuItem name="3-1" class="myitem"><svg-icon iconClass="denlu"></svg-icon >个人中心</MenuItem > -->
+            </Col>
+            <Col span="6"></Col>
+            <Col span="6"></Col>
           </Row>
         </div>
       </Col>
     </Row>
 
     <div class="center">
-     <router-view></router-view>
+      <router-view></router-view>
+      <OnePage></OnePage>
     </div>
   </div>
 </template>
 
 <script>
 
+import LoginRegister from "./LoginRegister.vue";
+import OnePage from "./OnePage.vue";
 export default {
+  components: {
+    OnePage,
+    LoginRegister,
+  },
   name: "HomePage",
 
- 
   data() {
     return {
       theme1: "light",
+      modal1: false,
     };
   },
 
   mounted() {},
 
   methods: {
-    shouye(){
+    shouye() {
+   
+
+    },
+    ok() {
       
-    }
+      this.$Message.info("Clicked ok");
+    },
+    cancel() {
+      this.$Message.info("Clicked cancel");
+    },
   },
 };
 </script>
 
 <style lang="css" scoped>
 .top {
-
   align-items: center;
 }
 .logo {
   height: 45px;
   width: 125px;
   margin-left: 20%;
-  /* background-color: aqua; */
 }
 .img {
   width: 8em;
   height: 45px;
 }
 
-.topright {
-
-  width: 27em;
-  height: 55px;
-  /* background-color: rgb(14, 14, 63); */
-  align-items: center;
-}
 .center {
-  height: 90vh;
+  height: 91vh;
   width: 100%;
   background-image: linear-gradient(to top, #ffffff 0%, #e7f5f5);
   box-shadow: inset 0px 15px 15px -18px rgb(171, 204, 237);
 }
-.icon{
-  width: 20px;
-  height: 20px;
+
+.tophead {
+  /* background-color: rgb(14, 14, 63); */
+  width: 27em;
+  height: 55px;
+  align-items: center;
+}
+.myitem {
+  display: flex;
+  justify-content: center;
+}
+.myitem:hover {
+  color: #6578f6;
 }
 
+.icon1{
+  height: 1.6em;
+  width: 1.6em;
+}
 </style>
+
+
--- src/router/router.js
+++ src/router/router.js
@@ -3,72 +3,66 @@ import VueRouter from "vue-router";
 Vue.use(VueRouter)
 // 防止连续点击多次路由报错
 const originalPush = VueRouter.prototype.push
-VueRouter.prototype.push = function push (location) {
+VueRouter.prototype.push = function push(location) {
   return originalPush.call(this, location).catch(err => err)
 }
 
-const routes =[
-    // {
-    //     path:'/',
-    //    redirect:"/login",
-    // },
-//     {
-//       path:'/register',
-//       name:"register",
-//       component:()=>import("../components/register.vue"),
-//    },
-  
-//    {
-//     path:'/login',
-//     name:"login",
-//     component:()=>import("../components/login.vue"),
-//  },
-//  {
-//     path:"/teacherHome",
-//     name:"teacherHome",
-//     component:()=>import("../components/TeacherModule/teacherHome.vue"),
-//     children:[
-//       {
-//          path:"Profile",
-//          name:"Profile",
-//          component:()=>import("../components/index.vue"),
-//       },
-//       {
-//          path:"Homepage",
-//          name:"Homepage",
-//          component:()=>import("../components/TeacherModule/Homepage.vue")
-//      },
-//       {
-//          path:"myCourse",
-//          name:"myCourse",
-//          component:()=>import("../components/TeacherModule/myCourse.vue")
-//       },
-//       {
-//          path:"myExperiment",
-//          name:"myExperiment",
-//          component:()=>import("../components/TeacherModule/myExperiment.vue")
-//       },
-//       {
-//          path:"onlineDemonstration",
-//          name:"onlineDemonstration",
-//          component:()=>import("../components/TeacherModule/onlineDemonstration.vue")
-//       }
-//     ]
-//  },
-//  {
-//     path:"/studentHome",
-//     name:"studentHome",
-//     component:()=>import("../components/StudentModule/studentHome.vue"),
-//  }
+const routes = [
+
+
+
+
+  {
+    path: '/mylogin',
+    name: "mylogin",
+    component: () => import("../components/myLogin.vue"),
+  },
+  //  {
+  //     path:"/teacherHome",
+  //     name:"teacherHome",
+  //     component:()=>import("../components/TeacherModule/teacherHome.vue"),
+  //     children:[
+  //       {
+  //          path:"Profile",
+  //          name:"Profile",
+  //          component:()=>import("../components/index.vue"),
+  //       },
+  //       {
+  //          path:"Homepage",
+  //          name:"Homepage",
+  //          component:()=>import("../components/TeacherModule/Homepage.vue")
+  //      },
+  //       {
+  //          path:"myCourse",
+  //          name:"myCourse",
+  //          component:()=>import("../components/TeacherModule/myCourse.vue")
+  //       },
+  //       {
+  //          path:"myExperiment",
+  //          name:"myExperiment",
+  //          component:()=>import("../components/TeacherModule/myExperiment.vue")
+  //       },
+  //       {
+  //          path:"onlineDemonstration",
+  //          name:"onlineDemonstration",
+  //          component:()=>import("../components/TeacherModule/onlineDemonstration.vue")
+  //       }
+  //     ]
+  //  },
+  //  {
+  //     path:"/studentHome",
+  //     name:"studentHome",
+  //     component:()=>import("../components/StudentModule/studentHome.vue"),
+  //  }
 
 ]
 
 const router = new VueRouter({
-    mode:'history',
-    base:process.env.BASE_URL,
-    routes,
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes,
 
 })
 
 export default router;
-   
+
